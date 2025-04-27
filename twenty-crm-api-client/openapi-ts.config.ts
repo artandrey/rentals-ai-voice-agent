9296679bbd4d956687b65ed8b529cc770c44954a
Artandrey <artandrey7777@gmail.com>
2025-04-27 21:27:42 +0300

    feat(twenty-crm-api-client): initialize API client with OpenAPI schema

---
+++ twenty-crm-api-client/openapi-ts.config.ts
@@ -0,0 +1,11 @@
+import { defineConfig } from '@hey-api/openapi-ts';
+
+export default defineConfig({
+  client: '@hey-api/client-fetch',
+
+  input: './schema.json',
+  output: {
+    path: './client',
+    clean: true,
+  },
+});
