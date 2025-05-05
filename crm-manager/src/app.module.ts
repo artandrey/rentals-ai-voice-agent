d1bdfd6248eed60bb5c17d91b51e1996aaf9ebb0
Artandrey <artandrey7777@gmail.com>
2025-05-05 18:12:03 +0300

    feat(crm-manager): integrate CrmModule into the main application module

--- crm-manager/src/app.module.ts
+++ crm-manager/src/app.module.ts
@@ -1,12 +1,14 @@
 import { Module } from '@nestjs/common';
 
+import { CrmModule } from '~modules/crm/infrastructure/nest/crm.module';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 
 @Module({
-  imports: [CoreModule, SharedModule],
+  imports: [CoreModule, SharedModule, CrmModule],
   controllers: [AppController],
   providers: [AppService],
 })
