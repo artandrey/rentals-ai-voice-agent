973eddf960c48b21b8958d093f4cdd2d15bac1f1
Artandrey <artandrey7777@gmail.com>
2025-05-05 15:47:56 +0300

    chore(twenty-crm-api-client): update package.json and tsup.config.ts for client structure and exports

--- twenty-crm-api-client/tsup.config.ts
+++ twenty-crm-api-client/tsup.config.ts
@@ -1,13 +1,17 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entry: ['client/index.ts'],
-  splitting: false,
+  entry: [
+    'client/index.ts',
+    'client/client.gen.ts',
+    'client/sdk.gen.ts',
+    'client/types.gen.ts',
+  ],
+  outDir: 'dist/client',
+  format: ['esm', 'cjs'],
+  dts: true,
   sourcemap: true,
   clean: true,
   minify: true,
-  dts: true,
-  outDir: 'dist',
-  format: ['esm', 'cjs'],
   tsconfig: 'tsconfig.json',
 });
