b6e0607da54d83abd12e742b87fbbe5d8cd08c2c
Artandrey <artandrey7777@gmail.com>
2025-05-08 16:29:22 +0300

    feat(agent): add rental availability date spans retrieval and integrate into rental listing output

--- agent/test_crm_client.py
+++ agent/test_crm_client.py
@@ -1,5 +1,6 @@
 import os
 
+from crm_api_client.crm_manager_client.api.rentals import rentals_controller_get_rental_available_date_spans
 from crm_api_client.crm_manager_client.api.clients import clients_controller_get_current_accommodation
 from crm_api_client.crm_manager_client.api.rentals import rentals_controller_get_rentals
 from crm_api_client.crm_manager_client.api.clients import clients_controller_find_client_by_phone, clients_controller_get_client_by_id
@@ -19,6 +20,12 @@ with Client(base_url=CRM_MANAGER_URL) as client:
     rentals_list =  rentals_controller_get_rentals.sync(
         client=client,
     )
+    rental_available_date_spans = rentals_controller_get_rental_available_date_spans.sync_detailed(
+        client=client,
+        id=rentals_list[0].id,
+        start_date="2025-05-10",
+        end_date="2025-05-15"
+    )
     print(f"Rentals list: {rentals_list}")
-
+    print(f"Rental available date spans: {rental_available_date_spans}")
  
